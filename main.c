f98401f6fb1013caae6b43ce9657e9bf029031fa
thddlrla <okjh0999@naver.com>
2023-10-08 19:06:56 +0900

    creat main c

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	int count =0;
+	int number =0;
+	
+	printf("input the string : ");
+	
+	while((c = getchar()) != '\n') {
+		
+		if (number =='2'){
+			number++;
+		}
+		else if(number=='0'){
+			number++;
+		}
+		else if(number =='1'){
+			number++;
+		}
+		else if(number =='8'){
+			number++;
+		}
+	}
+	printf("the number of digit : ", num);
+
+	return 0;
+}
